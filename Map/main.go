5ed073a4d3c93c568d9423239b83a6c9e932a92d
@RaginiSharma01 <raginisharma.r01@gmail.com>
2026-02-25 16:57:25 +0530

    maps

--- Map/main.go
+++ Map/main.go
@@ -10,7 +10,13 @@ type Student struct {
 
 func main() {
 	m := make(map[string]Student)
+	imap := make(map[string]interface{})
 
+	imap["name"] = "rahul"
+	imap["age"] = 30
+	imap["active"] = true
+
+	fmt.Println(imap)
 	m["s1"] = Student{"ragini", 1, 90}
 	m["s2"] = Student{"anmol", 2, 88}
 	s := m["s1"]
