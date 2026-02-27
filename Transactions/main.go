e2516a8082ccf191ad105d1f662d77183359eafb
@RaginiSharma01 <raginisharma.r01@gmail.com>
2026-02-27 10:27:53 +0530

    transaction handling and indexing

---
+++ Transactions/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func main() {
+
+	ctx := context.Background()
+
+	connStr := "postgres://postgres:1234@localhost:5432/GoBasics"
+
+	pool, err := pgxpool.New(ctx, connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer pool.Close()
+
+	tx, err := pool.Begin(ctx)
+	if err != nil {
+		log.Fatal("not able to start transaction:", err)
+	}
+	defer tx.Rollback(ctx)
+
+	_, err = tx.Exec(ctx,
+		"INSERT INTO orders (user_id, amount) VALUES ($1, $2)",
+		1, 15000,
+	)
+	if err != nil {
+		log.Println("Insert failed:", err)
+		return
+	}
+
+	err = tx.Commit(ctx)
+	if err != nil {
+		log.Println("Commit failed:", err)
+		return
+	}
+
+	log.Println("Transaction successful!")
+}
